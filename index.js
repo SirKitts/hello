26cfddf9a0d213be802e50e2998b1f13441166e0
sirkitts <sirkitts@gmail.com>
2024-03-04 19:22:42 +1100

    fix local time

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
+function getDateAndTimeToday() {
+  const date = new Date();
+  const time = new Date().toTimeString().split(" ");
+  
+  const month = date.getMonth()+1;
+  const monthStr = (month < 10) ? "0"+month : month;
+
+  const day = date.getDate();
+  const dayStr = (day < 10) ? "0"+day : day;
+
+  let offset = "0";
+  if (time[1].includes("+")) {
+    offset = Math.abs(parseInt(time[1].split("+")[1].substring(0,2)));
+  } else {
+    offset = -Math.abs(parseInt(time[1].split("-")[1].substring(0,2)));
+  }
+
+  const hrs = Math.abs(date.getHours() + offset);
+  const mins = date.getMinutes();
+  const secs = date.getSeconds();
+  const hrsStr = (hrs < 10) ? "0"+hrs : hrs;
+  const minsStr = (mins < 10) ? "0"+mins : mins;
+  const secsStr = (secs < 10) ? "0"+secs : secs;
+
+  const currentDate = [date.getFullYear(), monthStr, dayStr].join("-");
+  const currentTime = [hrsStr, minsStr, secsStr].join(":");
+  const localeDateTime = [currentDate, currentTime].join(" ");
+
+  return new Date(localeDateTime);
+};
+
 function getDayGreetings() {
-  const d = new Date();
-  const hour = d.getHours();
+  let today = new Date(getDateAndTimeToday());
+  let time = new Date().toTimeString().split(" ");
+
+  let offset = "0";
+  if (time[1].includes("+")) {
+    offset = Math.abs(parseInt(time[1].split("+")[1].substring(0,2)));
+  } else {
+    offset = -Math.abs(parseInt(time[1].split("-")[1].substring(0,2)));
+  }
+  
+  let hour = today.getHours() - offset;
+  if (hour < 0 ) {
+    hour = hour * -1;
+  }
 
   if (hour >= 17 ) {
     return 'Good evening'
